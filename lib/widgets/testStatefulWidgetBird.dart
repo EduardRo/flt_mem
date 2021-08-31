ea06c02d122e0f92bab60d1350f65985626276e4
EduardRo <eduardstancu@yahoo.com>
2021-08-31 10:12:07 +0300

    some tests

--- lib/widgets/testStatefulWidgetBird.dart
+++ lib/widgets/testStatefulWidgetBird.dart
@@ -1,30 +1,61 @@
 import 'package:flutter/material.dart';
+
 // this is a test --- must be deleted at the end
+void main() => runApp(ItemCounter());
 
-class Bird extends StatefulWidget {
-  const Bird({Key? key, this.color = const Color(0xFFFFE306), this.child})
-      : super(key: key);
+class ItemCounter extends StatefulWidget {
+  final String name;
+  ItemCounter({this.name = 'name'});
 
-  final Color color;
-  final Widget? child;
   @override
-  _BirdState createState() => _BirdState();
+  _ItemCounterState createState() => _ItemCounterState();
 }
 
-class _BirdState extends State<Bird> {
-  double _size = 1.0;
-  void grow() {
-    setState(() {
-      _size += 0.1;
-    });
-  }
+class _ItemCounterState extends State<ItemCounter> {
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: widget.color,
-      transform: Matrix4.diagonal3Values(_size, _size, 1.0),
-      child: widget.child,
+      child: Column(
+        children: [
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  count++;
+                });
+              },
+              child: Text(
+                '${widget.name}:$count',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: getMyColor(count)),
+              ),
+            ),
+          ),
+          Expanded(child: theWidget(count)),
+        ],
+      ),
     );
   }
+}
+
+Color getMyColor(int moneyCounter) {
+  if (moneyCounter > 5) {
+    return Colors.blue;
+  } else {
+    return Colors.redAccent;
+  }
+}
+
+Widget theWidget(int counter) {
+  int cont = counter;
+  print(cont);
+  return Container(
+    child: Text(
+      'myWidget is show!',
+      style: TextStyle(color: cont > 2 ? Colors.green : Colors.yellow),
+      textDirection: TextDirection.ltr,
+    ),
+  );
 }
